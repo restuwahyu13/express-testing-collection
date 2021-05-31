10f5d77f054b7ccfabb26d33618e0e2be5ef6dd1
restuwahyu13 <restuwahyu13@gmail.com>
2021-05-31 20:42:10 +0700

    publish files

--- express-jest-tdd-ts/src/__tests__/integration-test/product.test.ts
+++ express-jest-tdd-ts/src/__tests__/integration-test/product.test.ts
@@ -1,13 +1,17 @@
-import request from 'supertest'
+import request, { SuperAgentTest } from 'supertest'
 import faker from 'faker'
 import { isType } from 'is-any-type'
 import app from '../../app'
 import productModel from '../../models/model.product'
 
-let data, id
+let data, id, server
 
 describe('[Integration Testing] - Product Controller', function () {
 	beforeAll(function () {
+		// setup server http request
+		server = request.agent(app) as SuperAgentTest
+
+		// create fake data for create new product
 		data = {
 			name: faker.commerce.productName(),
 			price: Math.abs(parseInt(faker.commerce.price(1000, 30000))),
@@ -16,15 +20,19 @@ describe('[Integration Testing] - Product Controller', function () {
 		}
 	})
 
-	afterAll(function () {
+	afterAll(function (done) {
 		// cleanup data after all test finish
 		data = {}
+
 		// cleanup specific collection after all test finish
 		productModel.db.dropCollection('products')
+
+		// exit after dropCollection success
+		done()
 	})
 
 	it('add new product', async function (done) {
-		const res = await request(app).post('/api/v1/product').set({ 'Content-Type': 'application/json' }).send(data)
+		const res = await server.post('/api/v1/product').set({ 'Content-Type': 'application/json' }).send(data)
 
 		expect(res.statusCode).toBe(201)
 		expect(res.get('Content-Type')).toMatch(/json/)
@@ -33,7 +41,7 @@ describe('[Integration Testing] - Product Controller', function () {
 	})
 
 	it('results product', async function (done) {
-		const res = await request(app).get('/api/v1/products').set({ 'Content-Type': 'application/json' })
+		const res = await server.get('/api/v1/products').set({ 'Content-Type': 'application/json' })
 
 		expect(res.statusCode).toBe(200)
 		expect(res.get('Content-Type')).toMatch(/json/)
@@ -44,9 +52,7 @@ describe('[Integration Testing] - Product Controller', function () {
 	})
 
 	it('result product', async function (done) {
-		const res = await request(app)
-			.get('/api/v1/product/' + id)
-			.set({ 'Content-Type': 'application/json' })
+		const res = await server.get('/api/v1/product/' + id).set({ 'Content-Type': 'application/json' })
 
 		expect(res.statusCode).toBe(200)
 		expect(res.get('Content-Type')).toMatch(/json/)
@@ -56,7 +62,7 @@ describe('[Integration Testing] - Product Controller', function () {
 	})
 
 	it('update product', async function (done) {
-		const res = await request(app)
+		const res = await server
 			.put('/api/v1/product/' + id)
 			.set({ 'Content-Type': 'application/json' })
 			.send(data)
@@ -68,9 +74,7 @@ describe('[Integration Testing] - Product Controller', function () {
 	})
 
 	it('delete product', async function (done) {
-		const res = await request(app)
-			.delete('/api/v1/product/' + id)
-			.set({ 'Content-Type': 'application/json' })
+		const res = await server.delete('/api/v1/product/' + id).set({ 'Content-Type': 'application/json' })
 
 		expect(res.statusCode).toBe(200)
 		expect(res.get('Content-Type')).toMatch(/json/)
